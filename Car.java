11c1bcffbd097ee1d18e7020eb4b6587efe13902
fufulame41 <luizz20garcia@gmail.com>
2025-04-23 15:15:39 -0400

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,29 @@
+public class Car {
+    private String carId;
+    private String brand;
+    private String model;
+    private double basePricePerDay;
+
+    public Car(String carId, String brand, String model, double basePricePerDay) {
+        this.carId = carId;
+        this.brand = brand;
+        this.model = model;
+        this.basePricePerDay = basePricePerDay;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getBasePricePerDay() {
+        return basePricePerDay;
+    }
+}
